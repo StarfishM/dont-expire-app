11bee9b9ed52f4515f6cbca90b0a10df15d88814
Merle Fischer <merle_canada@hotmail.com>
2019-09-22 09:37:44 +0200

    adds basic search functionality

--- due-date-app/src/App.js
+++ due-date-app/src/App.js
@@ -1,23 +1,46 @@
-import React, { Component } from 'react';
-import {BrowserRouter, Route} from 'react-router-dom'
-import HelloWorldText from './HelloWorld';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import HelloWorldText from "./HelloWorld";
 import StockPantry from "./app/stockPantry";
-
+import Calendar from "react-calendar";
+import Pantry from "./app/pantry";
+import SearchItems from "./app/searchItems";
 export default class App extends Component {
-    constructor(){
-        super();
-        this.state ={}
-    }
-    componentDidMount(){
-        console.log("APP component mounted");
-    }
-    render() {
-        return (
-            <React.Fragment>
-            <BrowserRouter>
-            <HelloWorldText name="Merle" />
-            <Route path="/stock-pantry" component={StockPantry} />
-            </BrowserRouter>
-            </React.Fragment>);
-    }
+  constructor() {
+    super();
+    this.state = {
+      date: new Date()
+    };
+  }
+  onChange = date => {
+    console.log("*******state", this.state);
+
+    this.setState({ date });
+  };
+  componentDidMount() {
+    console.log("APP component mounted");
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <BrowserRouter>
+          <HelloWorldText name="Merle" />
+          <a href="/logout">Logout</a>
+          <Route path="/pantry" component={Pantry} />
+          <Route path="/search" component={SearchItems} />
+          <Route path="/stock-pantry" component={StockPantry} />
+          <Route
+            path="/calendar"
+            component={Calendar}
+            onChange={this.onChange}
+            value={this.state.date}
+          />
+        </BrowserRouter>
+      </React.Fragment>
+    );
+  }
 }
+
+//
+//
+//
