3115bd356c6c46c14b1d15c5db072778147b7aad
Merle Fischer <merle_canada@hotmail.com>
2020-07-23 10:22:53 +0200

    removes unnecessary directory with an earlier stage of the applictaion

--- due-date-app/src/app/calendar.js
+++
@@ -1,2 +0,0 @@
-import React, { Component } from "react";
-import Calendar from "react-calendar";
--- due-date-app/src/app/stockPantry.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function StockPantry(){
-    return (<h1>I AM THE STOCK PANTRY COMPONENT!</h1>);
-}
